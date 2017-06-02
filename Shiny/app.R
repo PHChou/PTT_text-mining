0cc451021006e2a634a91a8ea81813d4e9cd310c
PHChou <s604180@gmail.com>
2017-06-02 21:35:21 +0800

    測試data,wordcloud2上傳至shinyapps

--- Shiny/app.R
+++ Shiny/app.R
@@ -3,17 +3,14 @@ mydata <- read.csv("www/art.data.csv")
 source("helper.R")
 
 
-
-library(magrittr)
 library(shiny)
+#library(magrittr)
 
-# Define UI for application that draws a histogram
+
 ui <- fluidPage(
   
-  # Application title
   titlePanel("Old Faithful Geyser Data"),
   
-  # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
       
@@ -30,25 +27,25 @@ ui <- fluidPage(
                     "男&女" = "both"),
                   selected = "both"),
       
-      selectInput("data.type", "資料類型",
+      selectInput("data.typee", "資料類型",
                   c("科目" = "subject.type",
                     "地點" = "location.type"
                   )),
       h5(strong("排除關鍵字")),
-      textInput("file2_search1",label = NA,value = '的'),
-      textInput("file2_search2",label = NA,value = NA),
-      textInput("file2_search3",label = NA,value = NA),
-      textInput("file2_search4",label = NA,value = NA),
-      textInput("file2_search5",label = NA,value = NA),
-      textInput("file2_search6",label = NA,value = NA)
+      textInput("exclude.1",label = NA,value = '的'),
+      textInput("exclude.2",label = NA,value = NA),
+      textInput("exclude.3",label = NA,value = NA),
+      textInput("exclude.4",label = NA,value = NA),
+      textInput("exclude.5",label = NA,value = NA),
+      textInput("exclude.6",label = NA,value = NA)
     ),
     
-    # Show a plot of the generated distribution
+
     mainPanel(
-      plotOutput("f.word.colud"),
+      wordcloud2Output("f.word.cloud"),
       tableOutput('ddata'),
       tableOutput("f.data"),
-      textOutput("f.txt")
+      textOutput("output")
     )
   )
 )
@@ -60,14 +57,33 @@ server <- function(input, output) {
                                                              "girl" = c('女','妹','姐','姊'),
                                         'both' = c("兄",'男','弟','哥','女','妹','姐','姊')) })
   
-  #output$ddata <- renderTable({ super_grepl( c( input$art.type ),rdata= mydata ,col = 1)  })
+  #output$ddata <- renderTable({ head(super_grepl( c( input$art.type ),rdata= mydata ,col = 1))  })
   
   
-    ##output$output<- renderText({jiebaR::segment("每天都要吃早餐", cc)} )
+  #output$output<- renderText({jiebaR::segment("每天都要吃早餐", cc)} )
   #output$f.txt <- renderText(gender())
-  used.data <- reactive({ super_grepl( c( input$art.type ),rdata= mydata ,col = 1) %>%
-                          super_grepl( gender(),rdata= . ,col = 3)  })
-  output$ddata <- renderTable({ head(  used.data() )})
+  used.data <- reactive({ super_grepl( gender(),rdata= super_grepl( c( input$art.type ),rdata= mydata ,col = 1) 
+                                       ,col = 3)  })
+  #used.data <- reactive({ super_grepl( c( input$art.type ),rdata= mydata ,col = 1) %>%
+  #                        super_grepl( gender(),rdata= . ,col = 3)  })
+  #不知道為何shiny沒辦法library(magrittr)
+  word.plot <- reactive({ super_jieba_cloud(rawdata  = used.data(),
+                                            filters1 = c('地點'), filters2= c('科目'), col1 = 3,  
+                                            stopword = c( 0:10,LETTERS,letters,'地點','附近',
+                                                          "上","科目",'4.','的','及',"與","二",
+                                                          '為主','中','科','請','安全','注意','自身',
+                                                          '站','財物',"或",'可','的','近','路','中',
+                                                          '等','區') ,
+                                            output_select = "cloud"  )  })
+  
+  output$f.word.cloud <- renderWordcloud2( { word.plot() }) 
+  
+  #data.type1 <- reactive 
+
+  
+  
+  
+  #output$ddata <- renderTable({ head(  used.data() )})
 
 }
 
